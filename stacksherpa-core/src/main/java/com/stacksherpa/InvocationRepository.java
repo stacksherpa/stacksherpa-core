60efb46212627d4878c1e6424d1c6318e0a7def7
Luis Gervaso <Luis_Gervaso@intuit.com>
2020-09-11 13:19:09 +0100

    feat: initial commit

--- stacksherpa-core/src/main/java/com/stacksherpa/InvocationRepository.java
+++
@@ -1,8 +0,0 @@
-package com.stacksherpa;
-
-import io.micronaut.data.annotation.Repository;
-import io.micronaut.data.jpa.repository.JpaRepository;
-
-@Repository
-public interface InvocationRepository extends JpaRepository<Invocation, String> {
-}
--- stacksherpa-core/src/main/java/com/stacksherpa/JobRepository.java
+++
@@ -1,8 +0,0 @@
-package com.stacksherpa;
-
-import io.micronaut.data.annotation.Repository;
-import io.micronaut.data.jpa.repository.JpaRepository;
-
-@Repository
-public interface JobRepository extends JpaRepository<Job, String> {
-}
